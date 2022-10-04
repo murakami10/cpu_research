dca179a46efbfa7cf2c4b8adec5126d0b2eb0abe
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-10-04 15:36:24 +0900

    Add script

--- script/cpu_k8s_default.py
+++ script/cpu_k8s_default.py
@@ -3,5 +3,6 @@ with open("./cpu.txt") as f:
     text = f.read()
 
 s = text.split()
+
 for i in range(9):
-    print(s[i * 50 + 40]+","+s[i * 50 + 48])
+    print(s[i * 30 + 24]+","+s[i * 30 + 28])
