68f0030457c6a53ab3ce7de86f8acad8656bad3f
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-10-07 02:49:32 +0900

    fix scritp

--- manifest/script/watcher.sh
+++ manifest/script/watcher.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
+$status=100
+
 while true;
 do
-   sleep 15;
-   kubectl top pods --selector='region=$1,app=$2' >> aaa.txt
+   sleep 1
+   $res=`kubectl top pods --selector="region=$1,app=$2"`
+   $usage=`awk 'NR != 1 {sum += substr($2, 0, index($2,"m")-1)} END {print sum/(NR-1)}' <<< "$res"`
+
+   if [ ${usage} -ge 400 && ${status} -ne 80 ]; then
+     kubectl apply -f vs-80.yaml
+     $status=80
+     echo "apply vs-80.yaml"
+     continue
+   fi
+
+   if [ ${usage} -ge 350 && ${status} -ne 100 ]; then
+     kubectl apply -f vs-100.yaml
+     $status=100
+     echo "apply vs-100.yaml"
+     continue
+   fi
+
 done;
