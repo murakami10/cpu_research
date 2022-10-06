3e0535913ec4e81d9a842f2199920f4357d3ee7f
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-10-07 03:57:27 +0900

    fix scritp

--- manifest/script/watcher.sh
+++ manifest/script/watcher.sh
@@ -8,14 +8,14 @@ do
    res=`kubectl top pods --selector="region=$1,app=$2" --use-protocol-buffers > /tmp/tmp_pods.txt`
    usage=`awk 'NR != 1 {sum += substr($2, 0, index($2,"m")-1)} END {print sum/(NR-1)}' < /tmp/tmp_pods.txt`
 
-   if [ ${usage} > 400 ] && [${status} != 80 ]; then
+   if [ ${usage} -ge 400 ] && [ ${status} -ne 80 ]; then
      kubectl apply -f vs-80.yaml
      status=80
      echo "apply vs-80.yaml"
      continue
    fi
 
-   if [ ${usage} < 350 ] && [ ${status} != 100 ]; then
+   if [ ${usage} -le 350 ] && [ ${status} -ne 100 ]; then
      kubectl apply -f vs-100.yaml
      status=100
      echo "apply vs-100.yaml"
