d8f160b2faf265e0881c23cf5ecb4f06bd438e25
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-10-13 03:26:11 +0900

    fix script

--- script/top.sh
+++ script/top.sh
@@ -4,6 +4,6 @@ while :
 do
   echo ""
   date
-  kubectl top pods $1 --use-protocol-buffers --containers
+  kubectl top pods $1 --use-protocol-buffers --containers --selector="region=az1,app=server"
   sleep 15
 done
