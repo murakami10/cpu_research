ff814d404e9982bb4086c6bcabaa8f60fb95b3fc
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-10-13 05:00:27 +0900

    fix exec_wrk.sh

--- script/cpu.py
+++ script/cpu.py
@@ -4,4 +4,4 @@ with open("./result/cpu.txt") as f:
 
 s = text.split()
 for i in range(14):
-    print(s[i * 50 + 40]+","+s[i * 50 + 48])
+    print(s[i * 50 + 40][:-1]+","+s[i * 50 + 48][:-1])
--- script/result.py
+++ script/result.py
@@ -3,6 +3,6 @@ with open("./result.txt") as f:
     text = f.read()
 
 s = text.split()
-for i in range(9):
-    print(s[i * 51 + 26]+"," + s[i * 51 + 35])
+for i in range(12):
+    print(s[i * 51 + 31] + "," +s[i * 51 + 26][:-2]+"," + s[i * 51 + 35])
 
