87fcfdcae9904c463217c84c7f9e9784b7fd3b9a
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-10-13 04:07:18 +0900

    fix exec_wrk.sh

--- script/top.sh
+++ script/top.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-while :
-do
+echo "" > result/cpu.txt
+
+for i in `seq 1 14`; do
   echo ""
   date
-  kubectl top pods $1 --use-protocol-buffers --containers
+  kubectl top pods $1 --use-protocol-buffers --containers >> result/cpu.txt
   sleep 15
 done
+
