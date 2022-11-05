03eb5a6372f3c5d8886273a112c8b6e02dec5307
村上恭哉 <mkkyyyy@isa3-dhcp-190-242.naist.jp>
2022-11-05 20:57:51 +0900

    fix

--- script/experiment2/exec_wrk.sh
+++ script/experiment2/exec_wrk.sh
@@ -3,7 +3,9 @@ echo "" > result/result.txt
 
 for i in `seq 1 2`; do
 
-  echo $i
+  $client=$(10 * $i)
+
+  echo "client: " $client
 
   kubectl exec -i $1 -- /bin/sh << 'EOC'
 
@@ -11,7 +13,7 @@ for i in `seq 1 2`; do
     echo ""
     echo $j "回目"
     date
-    wrk -t 1 -c 30 -d 5 http://server:80/
+    wrk -t 1 -c client -d 2 http://server:80/
     date
   done
 EOC
--- script/experiment2/top.sh
+++ script/experiment2/top.sh
@@ -8,22 +8,22 @@ for i in `seq 1 2`; do
 
   echo "" > result/cpu_$i.txt
 
-  for j in `seq 1 2`; do
+  for j in `seq 1 8`; do
     echo $j
     echo "" >> result/cpu_$i.txt
     date  >> result/cpu_$i.txt
     kubectl top pods $1 --use-protocol-buffers --containers >> result/cpu_$i.txt
-    sleep 5
+    sleep 10
   done
 
   kubectl apply -f vs.yaml
 
-  for j in `seq 1 2`; do
+  for j in `seq 1 8`; do
     echo $j
     echo "" >> result/cpu_$i.txt
     date  >> result/cpu_$i.txt
     kubectl top pods $1 --use-protocol-buffers --containers >> result/cpu_$i.txt
-    sleep 5
+    sleep 10
   done
 
   kubectl apply -f vs_default.yaml
