f9ace8978287ebeaab70e8c826d82e9a2b10e82d
村上恭哉 <mkkyyyy@isa3-dhcp-190-242.naist.jp>
2022-11-05 19:57:19 +0900

    fix exec_wrk

--- script/experiment2/exec_wrk.sh
+++ script/experiment2/exec_wrk.sh
@@ -1,12 +1,26 @@
 echo "" > result/result.txt
 
-kubectl exec -i $1 -- /bin/sh << 'EOC'
+
 for i in `seq 1 12`; do
+
+  kubectl exec -i $1 -- /bin/sh << 'EOC'
+
+  for j in `seq 1 12`; do
+    echo ""
+    echo $j "回目"
+    date
+    wrk -t 1 -c 30 -d 10 http://server:80/
+    date
+  done
+
+  exit
+
   echo ""
-  echo $i "回目"
-  date
-  wrk -t 1 -c 30 -d 10 http://server:80/
-  date
+  echo ""
+  echo "-----------------------------"
+  echo ""
+  echo $i + 1
+  echo ""
+
 done
-'EOC'
 
