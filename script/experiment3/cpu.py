016b6138cb7e65fd9c86a0b25804e361e75a336e
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-11-14 21:01:35 +0900

    Fix script to calc metrics

--- script/experiment3/cpu.py
+++ script/experiment3/cpu.py
@@ -3,7 +3,7 @@ with open("result/cpu_1.txt") as f:
     text = f.read()
 
 s = text.split()
-for i in range(19):
+for i in range(11):
     print(s[i * 50 + 40][:-1]+","+s[i * 50 + 48][:-1])
 
 print("")
@@ -14,7 +14,7 @@ with open("result/cpu_2.txt") as f:
     text = f.read()
 
 s = text.split()
-for i in range(19):
+for i in range(11):
     print(s[i * 50 + 40][:-1]+","+s[i * 50 + 48][:-1])
 
 print("")
@@ -24,7 +24,7 @@ with open("result/cpu_3.txt") as f:
     text = f.read()
 
 s = text.split()
-for i in range(19):
+for i in range(11):
     print(s[i * 50 + 40][:-1]+","+s[i * 50 + 48][:-1])
 
 print("")
@@ -34,5 +34,25 @@ with open("result/cpu_4.txt") as f:
     text = f.read()
 
 s = text.split()
-for i in range(19):
+for i in range(11):
+    print(s[i * 50 + 40][:-1]+","+s[i * 50 + 48][:-1])
+
+print("")
+print("")
+
+with open("result/cpu_5.txt") as f:
+    text = f.read()
+
+s = text.split()
+for i in range(11):
+    print(s[i * 50 + 40][:-1]+","+s[i * 50 + 48][:-1])
+
+print("")
+print("")
+
+with open("result/cpu_6.txt") as f:
+    text = f.read()
+
+s = text.split()
+for i in range(11):
     print(s[i * 50 + 40][:-1]+","+s[i * 50 + 48][:-1])
--- script/experiment3/result.py
+++ script/experiment3/result.py
@@ -3,6 +3,6 @@ with open("result/result.txt") as f:
     text = f.read()
 
 s = text.split()
-for i in range(17):
+for i in range(8):
     print(s[i * 51 + 42] + "," +s[i * 51 + 26][:-2]+"," + s[i * 51 + 35])
 
