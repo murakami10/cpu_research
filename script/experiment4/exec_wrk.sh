a8a32cff7057bba7f9866c630eee4d85258a3e85
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-12-10 07:08:48 +0900

    fix shell

--- script/experiment4/exec_wrk.sh
+++ script/experiment4/exec_wrk.sh
@@ -1,34 +1,37 @@
+
 echo "" > result/result.txt
 
+i=$2
+
+(
 kubectl exec -i $1 -- /bin/sh <<  'EOC'
 
-  for i in `seq 1 7`; do
 
-   client=$((10 * $i))
-#  client=$((10 * ($i+7)))
+  client=$((10 * $i))
 
-    echo "client: " $client
+  echo "client: " $client
 
-    for j in `seq 1 8`; do
-      echo ""
-      echo $j "回目"
-      date
-      wrk -t 1 -c $client -d 10 http://server:80/
-      date
-    done
-
-    sleep 41.6
-
-
+  for j in `seq 1 8`; do
     echo ""
-    echo ""
-    echo ""
-    echo "----------------------------------------------------------------------------"
-    echo ""
-    echo ""
-    echo ""
-
+    echo $j "回目"
+    date
+    wrk -t 1 -c $client -d 10 http://server:80/
+    date
   done
 
+  sleep 41.6
+
+
+  echo ""
+  echo ""
+  echo ""
+  echo "----------------------------------------------------------------------------"
+  echo ""
+  echo ""
+  echo ""
+
+
 EOC
+
+) > text.txt
 
---
+++ script/experiment4/main.sh
@@ -0,0 +1,8 @@
+
+
+
+for i in `seq 1 2`; do
+
+  bash exec_wrk.sh $1 i
+
+done
