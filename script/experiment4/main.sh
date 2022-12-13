8ef948b78bfaf37fb51227a4910bba54a8c2bcb6
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-12-13 19:05:14 +0900

    fix scritp

--- script/experiment4/main.sh
+++ script/experiment4/main.sh
@@ -1,71 +1,20 @@
 
-echo "" > result/91/result/input.txt
-echo "" > result/91/latency/latency.txt
+array=(91 82 73 64)
 
+for portion in ${array[@]}; do
 
-for i in `seq 1 14`; do
+  echo "" > result/$portion/result/input.txt
+  echo "" > result/$portion/latency/latency.txt
 
-  bash exec_wrk.sh $1 $i 91 &
-  bash top.sh $i 91 &
-   bash latency.sh 91 &
-#  bash latency96.sh 91 &
+  for i in `seq 1 14`; do
 
-  wait
+    bash exec_wrk.sh $1 $i $portion &
+    bash top.sh $i $portion &
+    bash latency.sh $portion &
+#    bash latency97.sh $portion &
 
-  sleep 30
+    wait
+    sleep 30
 
+  done
 done
-
-
-echo "" > result/82/result/input.txt
-echo "" > result/82/latency/latency.txt
-
-
-for i in `seq 1 14`; do
-
-  bash exec_wrk.sh $1 $i 82 &
-  bash top.sh $i 82 &
-   bash latency.sh 82 &
-#  bash latency96.sh 82 &
-
-  wait
-
-  sleep 30
-
-done
-
-
-echo "" > result/73/result/input.txt
-echo "" > result/73/latency/latency.txt
-
-
-for i in `seq 1 14`; do
-
-  bash exec_wrk.sh $1 $i 73 &
-  bash top.sh $i 73 &
-   bash latency.sh 73 &
-#  bash latency96.sh 73 &
-
-  wait
-
-  sleep 30
-
-done
-
-echo "" > result/64/result/input.txt
-echo "" > result/64/latency/latency.txt
-
-
-for i in `seq 1 14`; do
-
-  bash exec_wrk.sh $1 $i 64 &
-  bash top.sh $i 64 &
-   bash latency.sh 64 &
-#  bash latency96.sh 64 &
-
-  wait
-
-  sleep 30
-
-done
-
