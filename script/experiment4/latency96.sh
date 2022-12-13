6a9c0188776a3bd404bcc490a74e7e0bf4a5760b
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-12-13 10:05:17 +0900

    fix scritp

--- script/experiment4/latency96.sh
+++ script/experiment4/latency96.sh
@@ -3,6 +3,10 @@
 
 for i in `seq 1 11`; do
 
+  echo "" >> result/$1/latency/latency.txt
+  echo $i "回目" >> result/$1/latency/latency.txt
+  echo "" >> result/$1/latency/latency.txt
+
   sleep 7
 
   s1LatencyP85=$(curl -s 'http://localhost:9090/api/v1/query' --data-urlencode 'query=histogram_quantile(0.97, sum(irate(istio_request_duration_milliseconds_bucket{reporter="source", app="client-node1", destination_workload="az1-server"}[1m])) by ( destination_canonical_service,destination_workload_namespace,source_canonical_service,source_workload_namespace, le ) )' | jq -r .data.result[0].value[1])
