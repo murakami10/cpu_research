dd59343d18b3968bfd389e77bfe242f26b1f51ea
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-12-13 09:49:25 +0900

    fix cpu.py

--- script/experiment4/result.py
+++ script/experiment4/result.py
@@ -15,9 +15,9 @@ for i, value in enumerate(text.split("------------------------------------------
     print(i+1)
     print()
 
-    t = re.sub(s, '', value)[2:].split()
+    t = re.sub(s, '', value).split()[2:]
     for i in range(8):
-        print(t[i * 51 + 44] + "," + t[i * 51 + 28][:-2] + "," + t[i * 51 + 37])
+        print(t[i * 50 + 41] + "," + t[i * 50 + 25][:-2] + "," + t[i * 50 + 34])
 
     print()
     print()
