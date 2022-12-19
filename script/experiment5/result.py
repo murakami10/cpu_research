a9157525b3d9103aaefc39a72a29c07516aaabab
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-12-19 09:23:22 +0900

    fix script

--- script/experiment5/result.py
+++ script/experiment5/result.py
@@ -1,23 +1,28 @@
 import re
 s = 'Socket.*?timeout \d+'
 
-portion = "91"
 
-with open(f"result/{portion}/result/input.txt") as f:
-    text = f.read()
+latency = "5"
+portions = ["9505", "9010", "8515", "8020", "7525", "7030", "6535", "6040", "5545", "5050", "00100"]
 
-for i, value in enumerate(text.split("----------------------------------------------------------------------------")):
-    if i == 14:
-        break
-    with open(f"result/{portion}/result/result_{i + 1}.txt", mode="w") as f:
-        f.write(value)
 
-    print(i+1)
-    print()
+for portion in portions:
+    with open(f"result/{latency}/{portion}/result/input.txt") as f:
+        text = f.read()
 
-    t = re.sub(s, '', value).split()[2:]
-    for i in range(8):
-        print(t[i * 50 + 41] + "," + t[i * 50 + 25][:-2] + "," + t[i * 50 + 34])
+    for i, value in enumerate(text.split("----------------------------------------------------------------------------")):
+        if i == 14:
+            break
 
-    print()
-    print()
+        with open(f"result/{latency}/{portion}/result/result_{i + 1}.txt", mode="w") as f:
+            f.write(value)
+
+        print(i+1)
+        print()
+
+        t = re.sub(s, '', value).split()[2:]
+        for i in range(8):
+            print(t[i * 50 + 41] + "," + t[i * 50 + 25][:-2] + "," + t[i * 50 + 34])
+
+        print()
+        print()
