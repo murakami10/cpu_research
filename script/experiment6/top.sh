f427748826b00f6426cf97fad7170bc66ccb7d01
村上恭哉 <mkkyyyy@mkkyyyy.local>
2023-01-02 20:14:10 +0900

    Add experiment6

--- script/experiment6/top.sh
+++ script/experiment6/top.sh
@@ -3,7 +3,7 @@
 echo "" > result/$1/cpu.txt
 
 #10ms
-portions=('_default' 9010 9010 9010 7030 7030 7030 5545 5545 5545 7030 7030 7030 8515 8515 8515 '_default' '_default' '_default' '_default' '_default' '_default' '_default' '_default')
+portions=('_default' 9010 9010 9010 7030 7030 7030 5545 5545 5545 5545 5545 5545 7030 7030 7030 8515 8515 8515 '_default' '_default' '_default' '_default' '_default' '_default' '_default' '_default')
 
 for j in `seq 1 24`; do
   echo "" >> result/$1/cpu.txt
