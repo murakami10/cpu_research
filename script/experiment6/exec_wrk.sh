593e1b244ebf71316db581dfddb29be061efbf15
村上恭哉 <mkkyyyy@mkkyyyy.local>
2023-01-02 17:52:11 +0900

    Add experiment6

--- script/experiment6/exec_wrk.sh
+++ script/experiment6/exec_wrk.sh
@@ -3,21 +3,134 @@ echo "" > result/$2/input.txt
 
 (
 
-kubectl exec -i $1 -- /bin/bash << 'EOC'
-
-clients=(10 20 30 40 50 60 70 80 90 100 110 120 130 140)
-
-for i in `seq 1 8` do
+kubectl exec -i $1 -- /bin/bash <<EOC
+  echo ''
+  echo '1回目'
+  date
+  wrk -t 1 -c 40 -d 10 http://server:80/
+  date
 
   echo ''
-  echo $i '回目'
+  echo '2回目'
   date
-  wrk -t 1 -c $clients[$i] -d 10 http://server:80/
+  wrk -t 1 -c 40 -d 10 http://server:80/
   date
 
-done
+  echo ''
+  echo '3回目'
+  date
+  wrk -t 1 -c 40 -d 10 http://server:80/
+  date
 
-'EOC'
+  echo ''
+  echo '4回目'
+  date
+  wrk -t 1 -c 80 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '5回目'
+  date
+  wrk -t 1 -c 80 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '6回目'
+  date
+  wrk -t 1 -c 80 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '7回目'
+  date
+  wrk -t 1 -c 120 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '8回目'
+  date
+  wrk -t 1 -c 120 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '9回目'
+  date
+  wrk -t 1 -c 120 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '10回目'
+  date
+  wrk -t 1 -c 140 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '11回目'
+  date
+  wrk -t 1 -c 140 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '12回目'
+  date
+  wrk -t 1 -c 140 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '13回目'
+  date
+  wrk -t 1 -c 80 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '14回目'
+  date
+  wrk -t 1 -c 80 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '15回目'
+  date
+  wrk -t 1 -c 80 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '16回目'
+  date
+  wrk -t 1 -c 50 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '17回目'
+  date
+  wrk -t 1 -c 50 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '18回目'
+  date
+  wrk -t 1 -c 50 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '19回目'
+  date
+  wrk -t 1 -c 10 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '20回目'
+  date
+  wrk -t 1 -c 10 -d 10 http://server:80/
+  date
+
+  echo ''
+  echo '21回目'
+  date
+  wrk -t 1 -c 10 -d 10 http://server:80/
+  date
+
+EOC
 
 echo ''
 echo ''
