5b611c3caaca4fc7a2069dc810f8f994a7027c6f
村上恭哉 <mkkyyyy@mkkyyyy.local>
2023-01-02 19:45:09 +0900

    Add experiment6

--- script/experiment6/top.sh
+++ script/experiment6/top.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-i=$1
-
 echo "" > result/$1/cpu.txt
+
+portions=(00100 00100 00100 00100 00100 00100 00100 00100 00100 00100 00100 00100 00100 00100 00100 00100 00100 00100 00100 00100)
 
 for j in `seq 1 23`; do
   echo "" >> result/$1/cpu.txt
   date  >> result/$1/cpu.txt
   kubectl top pods --use-protocol-buffers --containers >>  result/$1/cpu.txt
   sleep 10
+  kubectl apply -f vs$portions[$j].yaml
 done
+
+kubectl apply -f vs_default.yaml
 
