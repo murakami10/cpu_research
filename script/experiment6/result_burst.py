184c9203840c45f397a41c508b39a4cabe384823
村上恭哉 <mkkyyyy@mkkyyyy.local>
2023-01-04 15:23:34 +0900

    Add experiment6

---
+++ script/experiment6/result_burst.py
@@ -0,0 +1,26 @@
+import re
+s = 'Socket.*?timeout \d+'
+
+latency = "10"
+
+cpus = []
+
+with open(f"result/{latency}/cpu.txt") as f:
+    cpu = f.read()
+
+c = cpu.split()
+for i in range(12):
+    cpus.append([c[i * 50 + 40][:-1], c[i * 50 + 48][:-1]])
+
+
+with open(f"result/{latency}/input.txt") as f:
+    inp = f.read()
+
+results = []
+
+t = re.sub(s, '', inp).split()[2:]
+for i in range(12):
+    results.append([t[i * 50 + 39], t[i * 50 + 23][:-2], t[i * 50 + 32]])
+
+for i in range(21):
+    print(cpus[i][0]+","+cpus[i][1]+","+results[i][0]+","+results[i][1]+","+results[i][2])
