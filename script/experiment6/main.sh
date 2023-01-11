7d0958c448d593e145f54073e3761b4d7b4dc546
村上恭哉 <mkkyyyy@mkkyyyy.local>
2023-01-11 12:08:28 +0900

    fix

--- script/experiment6/main.sh
+++ script/experiment6/main.sh
@@ -1,7 +1,7 @@
 
 latency=10
 
-kubectl apply -f vs_xxxxx.yaml
+kubectl apply -f vsxxxxx.yaml
 
 sleep 10
 
@@ -10,22 +10,26 @@ bash top.sh $latency "dynamic" &
 
 wait
 
-#kubectl apply -f vs_9010.yaml
+#kubectl apply -f vs9010.yaml
+#sleep 10
 #bash exec_wrk.sh $1 $latency 9010 &
 #bash top_fix.sh $latency 9010 &
 #wait
 
-#kubectl apply -f vs_8020.yaml
+#kubectl apply -f vs8020.yaml
+#sleep 10
 #bash exec_wrk.sh $1 $latency 8020 &
 #bash top_fix.sh $latency 8020 &
 #wait
 
-#kubectl apply -f vs_7030.yaml
+#kubectl apply -f vs7030.yaml
+#sleep 10
 #bash exec_wrk.sh $1 $latency 7030 &
 #bash top_fix.sh $latency 7030 &
 #wait
 
-#kubectl apply -f vs_6040.yaml
+#kubectl apply -f vs6040.yaml
+#sleep 10
 #bash exec_wrk.sh $1 $latency 6040 &
 #bash top_fix.sh $latency 6040 &
 #wait
--- script/experiment6/main_burst.sh
+++ script/experiment6/main_burst.sh
@@ -1,7 +1,7 @@
 
 latency=10
 
-kubectl apply -f vs_xxxxx.yaml
+kubectl apply -f vsxxxxx.yaml
 
 sleep 10
 
@@ -9,22 +9,26 @@ bash exec_wrk_l10_burst.sh $1 $latency "dynamic" &
 bash top_burst.sh $latency "dynamic" &
 wait
 
-#kubectl apply -f vs_9010.yaml
+#kubectl apply -f vs9010.yaml
+#sleep 10
 #bash exec_wrk_l10_burst.sh $1 $latency 9010 &
 #bash top_burst_fix.sh $latency 9010 &
 #wait
 
-#kubectl apply -f vs_8020.yaml
+#kubectl apply -f vs8020.yaml
+#sleep 10
 #bash exec_wrk_l10_burst.sh $1 $latency 8020 &
 #bash top_burst_fix.sh $latency 8020 &
 #wait
 
-#kubectl apply -f vs_7030.yaml
+#kubectl apply -f vs7030.yaml
+#sleep 10
 #bash exec_wrk_l10_burst.sh $1 $latency 7030 &
 #bash top_burst_fix.sh $latency 7030 &
 #wait
 
-#kubectl apply -f vs_6040.yaml
+#kubectl apply -f vs6040.yaml
+#sleep 10
 #bash exec_wrk_l10_burst.sh $1 $latency 6040 &
 #bash top_burst_fix.sh $latency 6040 &
 #wait
